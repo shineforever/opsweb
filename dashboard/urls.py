e51406478a11d3ea05527b8b64c282e4d128f430
shine_forever <shine_forever@yeah.net>
2017-07-29 09:35:24 +0800

    login 和 logout在view里面的方式由函数，修改为类的方式

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,11 +5,14 @@ __date__ = '2017/7/18 10:11'
 from django.conf.urls import include, url
 from . import views
 
+
+
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^hello/$', views.hello),
-    url(r'^login/$', views.login_view),
-    url(r'^logout/$', views.logout_view),
+    url(r'^$', views.IndexView.as_view()),
+    url(r'^login/$', views.LoginView.as_view()),
+    url(r'^logout/$', views.LogoutView.as_view()),
+    # url(r'^hello/$', views.hello),
+    # url(r'^logout/$', views.logout_view),
     url(r'^test_form/$', views.test_form),
 
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from django.contrib.auth import authenticate,login,logout
 
+from django.views.generic import View
+
 # Create your views here.
 
 def hello(request):
@@ -14,50 +16,91 @@ def hello(request):
     """
     return HttpResponse("hello world!!!")
 
-def index(request):
-    return render(request,'public/index.html')
+class IndexView(View):
+    def get(self,request):
+        # template_name = "public/index.html"
+        return render(request,'public/index.html')
 
-def login_view(request):
-    """
-    登录页面
-    :param request: 
-    :return: 
-    """
-    if request.method == 'GET':
+
+
+class LoginView(View):
+    def get(self,request):
         return render(request, 'user/login.html', {"title": "reboot 运维平台"})
-    if request.method == 'POST':
-        ret = {"status": 0}
-        username = request.POST.get('username',None)
-        password = request.POST.get('password',None)
 
-        #验证用户名和密码
-        user = authenticate(username=username,password=password)
+    def post(self,request):
+        res = {"status": 0}
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        # 验证用户名和密码
+        user = authenticate(username=username, password=password)
 
         if user is not None:
             print user
+            print password
             if user.is_active:
-                login(request,user)  #传入的是user对象，而不是username
-
+                login(request, user)  # 传入的是user对象，而不是username
+                res['next_url'] = "/"
                 # return HttpResponse('login success')
             else:
-                ret['status'] = 1
-                ret['errmsg'] = "user is block"
+                res['status'] = 1
+                res['errmsg'] = "user is block"
 
                 # return HttpResponse('user is block')
         else:
-            ret['status'] = 2
-            ret['errmsg'] = "user or password is wrong!"
-        return JsonResponse(ret, safe=True)
+            res['status'] = 2
+            res['errmsg'] = "user or password is wrong!"
+        return JsonResponse(res, safe=True)
+
+
+class LogoutView(View):
+    def get(self,request):
+        logout(request)
+        return HttpResponse('user logout')
+
+# def login_view(request):
+#     """
+#     登录页面
+#     :param request:
+#     :return:
+#     """
+#     if request.method == 'GET':
+#         return render(request, 'user/login.html', {"title": "reboot 运维平台"})
+#     if request.method == 'POST':
+#         res = {"status": 0}
+#         username = request.POST.get('username',None)
+#         password = request.POST.get('password',None)
+#
+#         #验证用户名和密码
+#         user = authenticate(username=username,password=password)
+#
+#         if user is not None:
+#             print user
+#             print password
+#             if user.is_active:
+#                 login(request,user)  #传入的是user对象，而不是username
+#                 res['next_url']="/"
+#                 # return HttpResponse('login success')
+#             else:
+#                 res['status'] = 1
+#                 res['errmsg'] = "user is block"
+#
+#                 # return HttpResponse('user is block')
+#         else:
+#             res['status'] = 2
+#             res['errmsg'] = "user or password is wrong!"
+#         return JsonResponse(res, safe=True)
             # return HttpResponse('user or password is wrong!')
 
-def logout_view(request):
-    """
-    账户登出
-    :param request: 
-    :return: 
-    """
-    logout(request)
-    return HttpResponse('user logout')
+# def logout_view(request):
+#     """
+#     账户登出
+#     :param request:
+#     :return:
+#     """
+#     logout(request)
+#     return HttpResponse('user logout')
+
 
 
 def test_form(request):
